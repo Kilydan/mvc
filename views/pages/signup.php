2d7d0f62d692ad0ceddb70bed085752c61c98353
Tim Hoenselaar <thoenselaar@emixion.nl>
2016-12-01 15:49:11 +0100

    hihih

--- views/pages/signup.php
+++ views/pages/signup.php
@@ -1,5 +1,5 @@
 
-<form action="/models/signup.php">
+<form action="/controllers/signup_controller.php">
     User Name:<input type="text" name="username" placeholder="User Name">
     Password:<input type="text" name="password" placeholder="Password">
 
